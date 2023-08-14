3685f4fa7d58a240bb399451eec641841e09a907
Joseph Masengesho <jmasengesho@microsoft.com>
2023-08-14 18:17:47 -0500

    updated the url to the right endpoint

--- src/Web/BlazorClient/Program.cs
+++ src/Web/BlazorClient/Program.cs
@@ -7,9 +7,9 @@ public class Program
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
 
-       // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });  //https://eshop.dev.mfgakspg.com
+       // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-        builder.Services.AddHttpClient<CatalogClient>(client => client.BaseAddress = new Uri("https://eshop.dev.mfgakspg.com/"));
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://eshop.dev.mfgakspg.com/") });
 
         var settings = await LoadSettingsFromHostAsync(builder.Services);
         builder.Services.AddSingleton(settings);
